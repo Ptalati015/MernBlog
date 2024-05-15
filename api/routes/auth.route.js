c4b5c937d03b4eeded2e234d52c1a27b9743a224
Ptalat2 <ptalat2@uic.edu>
2024-05-14 23:45:15 -0500

    create signup api route

---
+++ api/routes/auth.route.js
@@ -0,0 +1,7 @@
+import  Express  from "express";
+import {signup} from "../controllers/auth.controller.js";
+const router = Express.Router();
+
+router.post("/signup", signup)  
+
+export default router; 
