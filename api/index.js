11f85c62ff3843e083a16069c28a0f75cde5727e
Ptalat2 <ptalat2@uic.edu>
2024-05-16 17:51:00 -0500

    add middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -28,4 +28,15 @@ app.get("/", (req, res) => {
 
 app.use('/api/user', userRoute);
 
-app.use('/api/auth', authRoute)
+app.use('/api/auth', authRoute);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode: statusCode,
+        message, 
+    })
+
+})
