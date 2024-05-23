32f0fce81743e608d755b76545c705a0406d34f5
Ptalat2 <ptalat2@uic.edu>
2024-05-22 19:36:32 -0500

    Footer completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Signup  from './pages/Signup'
 import Dashboard  from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
-
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -28,6 +28,7 @@ export default function App() {
 
 
       </Routes>
+      <Footer></Footer>
     
       </BrowserRouter>
   )
